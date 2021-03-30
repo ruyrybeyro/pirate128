8c6cbeeb17df892e7f545fb25c030cef1bd07a8f
Rui Ribeiro <ruyrybeyro@gmail.com>
2021-03-30 09:07:03 +0100

    comments updated

--- pirata1984/pirata.asm
+++ pirata1984/pirata.asm
@@ -347,6 +347,7 @@ DLOOP:		DEC	BC		; decrement BC $FFFF/65535 times
 		JR	NZ,DLOOP	; JR if BC not equal 0
 		RET
 
-;FFEF		15 bytes for stack, so 14 available without corrupting machine code
-;		(SP should be initialized to 0 and not $FFFF for using 16 bytes)
+;FFEF		16 bytes at then end of RAM empty
+;               15 bytes for stack from SP, so 14 available without corrupting machine code
+;		(SP should be initialized to 0 and not $FFFF for using the 16 allocated bytes)
 		
